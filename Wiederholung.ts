f4097761e2d180c34e5443b4921f892d2f0746ec
haager-ca <caius.haager@icloud.com>
2022-10-13 19:30:56 +0200

    new_Repetition

---
+++ Wiederholung.ts
@@ -0,0 +1,33 @@
+class Drohne {
+    private typ: string;
+    private axles: number = 4;
+    private maxBatteryCapacity: number = 1550;
+
+    constructor(typ: string = "Armattan") {
+        this.typ = typ;
+    }
+}
+
+class LoadingStation {
+    private drohnen: Drohne[] = []
+    private ort: string;
+
+
+    constructor(ort: string = "München") {
+        this.ort = ort;
+    }
+
+}
+
+const drohne1 = new Auto();
+const drohne2 = new Auto("Mercedes");
+const drohne3 = new Auto("BMW");
+
+const loadingstation = new LoadingStation();
+
+for (let i = 0; i < 10; i++) {
+    const autoTesla = new Auto();
+    parkhaus.autosHinzufuegen(autoTesla);
+}
+
+console.log(parkhaus);
